882d3454df17bcb3e1b479bd4d19f64f9d03a9b7
Théo OIRY <theo.oiry@yahoo.fr>
2021-03-12 10:34:23 +0100

    change check-style

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -2,9 +2,9 @@ package com.epsi.mycal;
 
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("woaw");
+    public static void main(String[] args) {
+        System.out.println("woaw");
 
-	}
+    }
 
 }
--- src/test/java/com/epsi/mycal/CalculatorTest.java
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -8,34 +8,34 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testAdd() {
+    @Test
+    public void testAdd() {
 
-		int a = 2;
-		int b = 3;
+        int a = 2;
+        int b = 3;
 
-		Calculator calc = new Calculator();
-		int actual = calc.add(a, b);
-		assertEquals(actual, 5);
-	}
+        Calculator calc = new Calculator();
+        int actual = calc.add(a, b);
+        assertEquals(actual, 5);
+    }
 
-	@Test
-	public void testSub() {
+    @Test
+    public void testSub() {
 
-		int a = 2;
-		int b = 3;
+        int a = 2;
+        int b = 3;
 
-		Calculator calc = new Calculator();
-		int actual = calc.sub(a, b);
-		assertEquals(actual, -1);
-	}
+        Calculator calc = new Calculator();
+        int actual = calc.sub(a, b);
+        assertEquals(actual, -1);
+    }
 
 }
